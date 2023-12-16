ad824bf22bfd5a0b9aab300772f6e01776fce8cc
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2023-12-17 00:45:30 +0300

    Solve day-01 part1

--- day-01/src/error.rs
+++ day-01/src/error.rs
@@ -6,4 +6,11 @@ pub enum AocError {
     #[error(transparent)]
     #[diagnostic(code(aoc::io_error))]
     IoError(#[from] std::io::Error),
+
+    #[error("no calibration value in `{0}`")]
+    #[diagnostic(code(aoc::no_calibration_value))]
+    NoCalibrationValue(String),
+
+    #[error("cannot parse `{0} as number")]
+    CannotParseAsNumber(String),
 }
--- day-01/src/part1.rs
+++ day-01/src/part1.rs
@@ -1,8 +1,25 @@
 use crate::error::AocError;
 
 #[tracing::instrument]
-pub fn process(_input: &str) -> miette::Result<String, AocError> {
-    todo!("day 01 - part 1");
+fn recover_calibration_value(line: &str) -> Result<u32, AocError> {
+    let mut digits = line.chars().filter_map(|c| c.to_digit(10));
+    let first = digits.next();
+    let last = digits.last().or(first);
+    let (Some(x), Some(y)) = (first, last) else {
+        return Err(AocError::NoCalibrationValue(line.to_string()));
+    };
+    format!("{x}{y}")
+        .parse()
+        .map_err(|_| AocError::CannotParseAsNumber(line.to_string()))
+}
+
+#[tracing::instrument]
+pub fn process(input: &str) -> miette::Result<String, AocError> {
+    input
+        .lines()
+        .map(recover_calibration_value)
+        .sum::<Result<u32, _>>()
+        .map(|r| r.to_string())
 }
 
 #[cfg(test)]
@@ -11,9 +28,16 @@ mod tests {
 
     #[test]
     fn test_process() -> miette::Result<()> {
-        todo!("haven't built test yet");
-        let input = "";
-        assert_eq!("", process(input)?);
+        // In this example, the calibration values of these four lines are:
+        // 12, 38, 15, and 77
+        // Adding these together produces 142
+
+        let input = "1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet";
+
+        assert_eq!("142", process(input)?);
         Ok(())
     }
 }
