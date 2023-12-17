9fa4e0b74ef4a98cb82fe2cdd106d9996f048883
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2023-12-17 16:20:04 +0300

    Failed attempt to use unfold for parsing lines

--- day-01/src/part2.rs
+++ day-01/src/part2.rs
@@ -1,19 +1,43 @@
 use crate::error::AocError;
 
 #[tracing::instrument]
-pub fn process(_input: &str) -> miette::Result<String, AocError> {
-    todo!("day 01 - part 2");
+fn recover_calibration_value(line: &str) -> Result<u32, AocError> {
+    let mut digits = line.chars().filter_map(|c| c.to_digit(10));
+    let first = digits.next();
+    let last = digits.last().or(first);
+    let (Some(x), Some(y)) = (first, last) else {
+        return Err(AocError::NoCalibrationValue(line.to_string()));
+    };
+    format!("{x}{y}")
+        .parse()
+        .map_err(|_| AocError::CannotParseAsNumber(line.to_string()))
+}
+
+#[tracing::instrument]
+pub fn process(input: &str) -> miette::Result<String, AocError> {
+    input
+        .lines()
+        .map(recover_calibration_value)
+        .sum::<Result<u32, _>>()
+        .map(|r| r.to_string())
 }
 
 #[cfg(test)]
 mod tests {
+    use rstest::rstest;
+
     use super::*;
 
-    #[test]
-    fn test_process() -> miette::Result<()> {
-        todo!("haven't built test yet");
-        let input = "";
-        assert_eq!("", process(input)?);
+    #[rstest]
+    #[case("two1nine", "29")]
+    #[case("eightwothree", "83")]
+    #[case("abcone2threexyz", "13")]
+    #[case("xtwone3four", "24")]
+    #[case("4nineeightseven2", "42")]
+    #[case("zoneight234", "14")]
+    #[case("7pqrstsixteen", "76")]
+    fn test_process(#[case] input: &str, #[case] expected: &str) -> miette::Result<()> {
+        assert_eq!(expected, process(input)?);
         Ok(())
     }
 }
--- scratch/src/main.rs
+++ scratch/src/main.rs
@@ -1,3 +1,5 @@
+#![allow(dead_code)]
+
 use miette::{miette, Context, Diagnostic, IntoDiagnostic, LabeledSpan, SourceSpan};
 use semver::Version;
 use thiserror::Error;
@@ -66,7 +68,7 @@ fn print_report() {
     println!("{:?}", report)
 }
 
-fn main() -> miette::Result<()> {
+fn demo_miette() {
     // print_report();
 
     // just_fail().wrap_err("you are zhostko naebalsya")?;
@@ -78,15 +80,105 @@ fn main() -> miette::Result<()> {
     //     .parse()
     //     .into_diagnostic()
     //     .wrap_err("Failed to parse semver version")?;
+}
 
-    let data = include_str!("./data.txt");
-    dbg!(data);
+fn demo_include_str() {
+    // let data = include_str!("./data.txt");
+    // dbg!(data);
+}
+
+fn main() -> miette::Result<()> {
+    // demo_miette();
+    // demo_include_str();
 
     Ok(())
 }
 
 #[cfg(test)]
 mod tests {
+    use itertools::unfold;
+
     #[test]
-    fn test_part1() {}
+    fn test_unfold_digit_names() {
+        let input = "xtwone3four";
+        let digits = unfold(String::from(input), |s| {
+            let (result, skip) = if s.starts_with("one") {
+                (Some('1'), 3)
+            } else if s.starts_with("two") {
+                (Some('2'), 3)
+            } else if s.starts_with("three") {
+                (Some('3'), 5)
+            } else if s.starts_with("four") {
+                (Some('4'), 4)
+            } else if s.starts_with("five") {
+                (Some('5'), 4)
+            } else if s.starts_with("six") {
+                (Some('6'), 3)
+            } else if s.starts_with("seven") {
+                (Some('7'), 5)
+            } else if s.starts_with("eight") {
+                (Some('8'), 5)
+            } else if s.starts_with("nine") {
+                (Some('9'), 4)
+            } else if let Some((_, c)) = s
+                .chars()
+                .next()
+                .and_then(|c| c.to_digit(10).map(|d| (d, c)))
+            {
+                (Some(c), 1)
+            } else {
+                (None, 1)
+            };
+
+            *s = String::from(&s[skip..]);
+
+            result
+        });
+        let result = digits.take(3).collect::<Vec<_>>();
+        assert_eq!(result, vec!['a']);
+    }
+
+    #[test]
+    fn test_unfold_konyacci() {
+        let konyacci = unfold((String::from("x"), String::from("y")), |(s1, s2)| {
+            let ss1 = format!("{s1}{s2}");
+            let ss2 = format!("{s2}{s1}");
+            *s1 = ss1.clone();
+            *s2 = ss2;
+            let next = ss1;
+            Some(next)
+        });
+        // itertools::assert_equal(konyacci.by_ref().take(6), vec!["xy"]);
+        assert_eq!(
+            konyacci.skip(2).take(3).collect::<Vec<String>>(),
+            vec![
+                "xyyxyxxy",
+                "xyyxyxxyyxxyxyyx",
+                "xyyxyxxyyxxyxyyxyxxyxyyxxyyxyxxy"
+            ]
+        )
+    }
+
+    #[test]
+    fn test_unfold_fibonacci() {
+        let mut fibonacci = unfold((1u32, 1u32), |(x1, x2)| {
+            // Attempt to get the next Fibonacci number
+            let next = x1.saturating_add(*x2);
+
+            // Shift left: ret <- x1 <- x2 <- next
+            let ret = *x1;
+            *x1 = *x2;
+            *x2 = next;
+
+            // If addition has saturated at the maximum, we are finished
+            if ret == *x1 && ret > 1 {
+                None
+            } else {
+                Some(ret)
+            }
+        });
+
+        itertools::assert_equal(fibonacci.by_ref().take(8), vec![1, 1, 2, 3, 5, 8, 13, 21]);
+        assert_eq!(fibonacci.last(), Some(2_971_215_073))
+    }
 }
