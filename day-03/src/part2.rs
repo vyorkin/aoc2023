2244d20352022198e2734e8c92c93f84be4e9e74
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2023-12-21 01:54:42 +0300

    Refactor #1

--- day-03/src/part2.rs
+++ day-03/src/part2.rs
@@ -1,61 +1,13 @@
+use crate::error::AocError;
+use itertools::Itertools;
 use std::collections::BTreeMap;
 
-use itertools::Itertools;
+type CharMatrix = Vec<Vec<char>>;
 
-use crate::error::AocError;
+struct EngineSchema(pub CharMatrix);
 
-#[inline]
-fn is_gear(c: char) -> bool {
-    c == '*'
-}
-
-#[tracing::instrument]
-pub fn process(input: &str) -> miette::Result<String, AocError> {
-    let matrix = input
-        .lines()
-        .map(|line| format!(".{line}.").chars().collect())
-        .collect::<Vec<Vec<char>>>();
-
-    let symbols = matrix
-        .iter()
-        .enumerate()
-        .flat_map(|(i, cells)| {
-            cells
-                .iter()
-                .enumerate()
-                .filter_map(|(j, &c)| if is_gear(c) { Some((i, j)) } else { None })
-                .collect::<Vec<_>>()
-        })
-        .collect::<Vec<(usize, usize)>>();
-
-    let numbers = matrix
-        .iter()
-        .enumerate()
-        .flat_map(|(i, cells)| {
-            cells
-                .iter()
-                .enumerate()
-                .fold((Vec::new(), None), |(mut acc, maybe_digit), (j, char)| {
-                    if let Some(prev_digit) = maybe_digit {
-                        if let Some(n) = char.to_digit(10) {
-                            (acc, Some(prev_digit * 10 + n))
-                        } else {
-                            acc.push(((i, j - 1), prev_digit));
-                            (acc, None)
-                        }
-                    } else if let Some(n) = char.to_digit(10) {
-                        (acc, Some(n))
-                    } else {
-                        (acc, None)
-                    }
-                })
-                .0
-        })
-        .collect::<BTreeMap<(usize, usize), u32>>();
-
-    // dbg!(numbers.clone());
-
-    let offsets: Vec<(i32, i32)> = vec![
+impl EngineSchema {
+    const OFFSETS: [(i32, i32); 8] = [
         (-1, -1),
         (-1, 0),
         (-1, 1),
@@ -66,48 +18,111 @@ pub fn process(input: &str) -> miette::Result<String, AocError> {
         (1, 1),
     ];
 
-    let gears = symbols
-        .into_iter()
-        .filter_map(|(i, j)| {
-            let r = offsets
-                .iter()
-                .unique()
-                .fold(Vec::new(), |mut gears_acc, (di, dj)| {
-                    let row = i as i32 + di;
-                    let col = j as i32 + dj;
+    pub fn new(input: &str) -> Self {
+        let result = input
+            .lines()
+            .map(|line| format!(".{}.", line).chars().collect())
+            .collect();
 
-                    let adjacent =
-                        numbers
-                            .iter()
-                            .fold(Vec::new(), |mut num_acc, (&(ni, nj), &n)| {
-                                let num_length = n.to_string().len() as i32;
-                                let num_row = ni as i32;
-                                let num_col_start = nj as i32 - num_length;
-                                let num_col_end = nj as i32;
+        Self(result)
+    }
 
-                                if num_row == row && num_col_start < col && col <= num_col_end {
-                                    num_acc.push(n);
-                                }
+    fn numbers(&self) -> BTreeMap<(usize, usize), u32> {
+        self.0
+            .iter()
+            .enumerate()
+            .flat_map(|(i, cells)| {
+                cells
+                    .iter()
+                    .enumerate()
+                    .fold((Vec::new(), None), |(mut acc, maybe_digit), (j, &char)| {
+                        if let Some(prev_digit) = maybe_digit {
+                            if let Some(n) = char.to_digit(10) {
+                                (acc, Some(prev_digit * 10 + n))
+                            } else {
+                                acc.push(((i, j - 1), prev_digit));
+                                (acc, None)
+                            }
+                        } else if let Some(n) = char.to_digit(10) {
+                            (acc, Some(n))
+                        } else {
+                            (acc, None)
+                        }
+                    })
+                    .0
+            })
+            .collect::<BTreeMap<(usize, usize), u32>>()
+    }
 
-                                num_acc
-                            });
+    fn gear_candidates(&self) -> Vec<(usize, usize)> {
+        self.0
+            .iter()
+            .enumerate()
+            .flat_map(|(i, cells)| {
+                cells
+                    .iter()
+                    .enumerate()
+                    .filter(|(_, &c)| Self::is_gear_marker(c))
+                    .map(move |(j, _)| (i, j))
+            })
+            .collect::<Vec<(usize, usize)>>()
+    }
 
-                    gears_acc.push(adjacent);
-                    gears_acc
-                })
-                .into_iter()
-                .flatten()
-                .unique()
-                .collect::<Vec<_>>();
+    pub fn gears(&self) -> Vec<u32> {
+        let numbers = self.numbers();
+        self.gear_candidates()
+            .into_iter()
+            .filter_map(|(i, j)| {
+                let r = Self::OFFSETS
+                    .iter()
+                    .unique()
+                    .fold(Vec::new(), |mut gears_acc, (di, dj)| {
+                        let row = i as i32 + di;
+                        let col = j as i32 + dj;
 
-            if r.len() == 2 {
-                Some(r.iter().product::<u32>())
-            } else {
-                None
-            }
-        })
-        .collect::<Vec<u32>>();
+                        let adjacent =
+                            numbers
+                                .iter()
+                                .fold(Vec::new(), |mut num_acc, (&(ni, nj), &n)| {
+                                    let num_length = n.to_string().len() as i32;
+                                    let num_row = ni as i32;
+                                    let num_col_start = nj as i32 - num_length;
+                                    let num_col_end = nj as i32;
 
+                                    if num_row == row && num_col_start < col && col <= num_col_end {
+                                        num_acc.push(n);
+                                    }
+
+                                    num_acc
+                                });
+
+                        gears_acc.push(adjacent);
+                        gears_acc
+                    })
+                    .into_iter()
+                    .flatten()
+                    .unique()
+                    .collect::<Vec<_>>();
+
+                if r.len() == 2 {
+                    Some(r.iter().product::<u32>())
+                } else {
+                    None
+                }
+            })
+            .collect::<Vec<u32>>()
+    }
+
+    #[inline]
+    fn is_gear_marker(c: char) -> bool {
+        c == '*'
+    }
+}
+
+#[tracing::instrument]
+pub fn process(input: &str) -> miette::Result<String, AocError> {
+    let engine_schema = EngineSchema::new(input);
+    let gears = engine_schema.gears();
     Ok(gears.iter().sum::<u32>().to_string())
 }
 
