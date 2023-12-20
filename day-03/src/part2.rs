76812bba55269d2b7e9819f118de710ca5c2ebfa
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2023-12-21 02:43:43 +0300

    Refactor #3

--- day-03/src/part2.rs
+++ day-03/src/part2.rs
@@ -3,9 +3,12 @@ use itertools::Itertools;
 
 type CharMatrix = Vec<Vec<char>>;
 
-struct EngineSchema(pub CharMatrix);
+struct EngineSchematic {
+    numbers: Vec<((usize, usize), u32)>,
+    gear_candidates: Vec<(usize, usize)>,
+}
 
-impl EngineSchema {
+impl EngineSchematic {
     const OFFSETS: [(i32, i32); 8] = [
         (-1, -1),
         (-1, 0),
@@ -18,16 +21,71 @@ impl EngineSchema {
     ];
 
     pub fn new(input: &str) -> Self {
-        let result = input
+        let matrix = input
             .lines()
             .map(|line| format!("{}.", line).chars().collect())
             .collect();
 
-        Self(result)
+        Self {
+            numbers: Self::numbers(&matrix),
+            gear_candidates: Self::gear_candidates(&matrix),
+        }
     }
 
-    fn numbers(&self) -> Vec<((usize, usize), u32)> {
-        self.0
+    pub fn calculate_gear_ratio_sum(&self) -> u32 {
+        self.gear_ratios()
+            .into_iter()
+            .map(|(x, y)| x * y)
+            .sum::<u32>()
+    }
+
+    pub fn gear_ratios(&self) -> Vec<(u32, u32)> {
+        self.gear_candidates
+            .iter()
+            .filter_map(|&(i, j)| {
+                let adjacent = self.adjacent_numbers(i, j);
+                if adjacent.len() == 2 {
+                    Some((adjacent[0], adjacent[1]))
+                } else {
+                    None
+                }
+            })
+            .collect()
+    }
+
+    fn adjacent_numbers(&self, i: usize, j: usize) -> Vec<u32> {
+        Self::OFFSETS
+            .iter()
+            .fold(Vec::new(), |mut acc, (di, dj)| {
+                let row = i as i32 + di;
+                let col = j as i32 + dj;
+                acc.push(self.adjacent_numbers_at(row, col));
+                acc
+            })
+            .into_iter()
+            .flatten()
+            .unique()
+            .collect()
+    }
+
+    fn adjacent_numbers_at(&self, row: i32, col: i32) -> Vec<u32> {
+        self.numbers
+            .iter()
+            .fold(Vec::new(), |mut acc, &((i, j), n)| {
+                let col_start = j as i32 - n.to_string().len() as i32;
+                let col_end = j as i32;
+                let in_range = i as i32 == row && col_start < col && col <= col_end;
+
+                if in_range {
+                    acc.push(n);
+                }
+
+                acc
+            })
+    }
+
+    fn numbers(matrix: &CharMatrix) -> Vec<((usize, usize), u32)> {
+        matrix
             .iter()
             .enumerate()
             .flat_map(|(i, cells)| {
@@ -53,8 +111,8 @@ impl EngineSchema {
             .collect()
     }
 
-    fn gear_candidates(&self) -> Vec<(usize, usize)> {
-        self.0
+    fn gear_candidates(matrix: &CharMatrix) -> Vec<(usize, usize)> {
+        matrix
             .iter()
             .enumerate()
             .flat_map(|(i, cells)| {
@@ -67,52 +125,6 @@ impl EngineSchema {
             .collect()
     }
 
-    pub fn gear_ratios(&self) -> Vec<(u32, u32)> {
-        let numbers = self.numbers();
-
-        self.gear_candidates()
-            .into_iter()
-            .filter_map(|(i, j)| {
-                let r = Self::OFFSETS
-                    .iter()
-                    .unique()
-                    .fold(Vec::new(), |mut gears_acc, (di, dj)| {
-                        let row = i as i32 + di;
-                        let col = j as i32 + dj;
-
-                        let adjacent =
-                            numbers
-                                .iter()
-                                .fold(Vec::new(), |mut num_acc, &((ni, nj), n)| {
-                                    let num_length = n.to_string().len() as i32;
-                                    let num_row = ni as i32;
-                                    let num_col_start = nj as i32 - num_length;
-                                    let num_col_end = nj as i32;
-
-                                    if num_row == row && num_col_start < col && col <= num_col_end {
-                                        num_acc.push(n);
-                                    }
-
-                                    num_acc
-                                });
-
-                        gears_acc.push(adjacent);
-                        gears_acc
-                    })
-                    .into_iter()
-                    .flatten()
-                    .unique()
-                    .collect::<Vec<_>>();
-
-                if r.len() == 2 {
-                    Some((r[0], r[1]))
-                } else {
-                    None
-                }
-            })
-            .collect()
-    }
-
     #[inline]
     fn is_gear_marker(c: char) -> bool {
         c == '*'
@@ -121,10 +133,9 @@ impl EngineSchema {
 
 #[tracing::instrument]
 pub fn process(input: &str) -> miette::Result<String, AocError> {
-    let engine_schema = EngineSchema::new(input);
-    let gear_ratios = engine_schema.gear_ratios();
-    let sum_of_gear_ratios = gear_ratios.into_iter().map(|(x, y)| x * y).sum::<u32>();
-    Ok(sum_of_gear_ratios.to_string())
+    let engine_schematic = EngineSchematic::new(input);
+    let result = engine_schematic.calculate_gear_ratio_sum();
+    Ok(result.to_string())
 }
 
 #[cfg(test)]
